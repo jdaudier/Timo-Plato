5b618926cb8c34d56b50352f0f60cd47f2c07e2d
Joanne Daudier <jdaudier@outlook.com>
2015-09-07 23:59:11 -0400

    Revert "FIX: add env vars"
    This reverts commit cd549ac5d94ddaa830ebce0b7490d4254397599b.

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-var env = process.env.NODE_ENV || 'dev';
 var path = require('path');
 var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config');
 var app = express();
-app.use(express.static('dist'));
+var compiler = webpack(config);
+require('colors');
+
 app.set('port', process.env.PORT || 8080);
 
-if (env === 'dev') {
-    var webpack = require('webpack');
-    var config = require('./webpack.config');
-    var compiler = webpack(config);
-    require('colors');
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
 
-    app.use(require('webpack-dev-middleware')(compiler, {
-        noInfo: true,
-        publicPath: config.output.publicPath
-    }));
+app.use(require('webpack-hot-middleware')(compiler));
 
-    app.use(require('webpack-hot-middleware')(compiler));
-}
+app.use(express.static('dist'));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -29,26 +27,24 @@ var server = app.listen(app.get('port'), function(err) {
         return;
     }
 
-    if (env === 'dev') {
-        console.log('      _______________________'.magenta);
-        console.log('()===(                      (@===()'.magenta);
-        console.log('     \'______________________\'|'.magenta);
-        console.log('       |                     |'.magenta);
-        console.log('       |  Timo Plato         |'.magenta);
-        console.log('       |  is                 |'.magenta);
-        console.log('       |  now                |'.magenta);
-        console.log('       |  running!           |'.magenta);
-        console.log('       |                     |'.magenta);
-        console.log('       |                     |'.magenta);
-        console.log('       |             love,   |'.magenta);
-        console.log('       |           Joanne    |'.magenta);
-        console.log('      _)_____________________|'.magenta);
-        console.log('()===(                      (@===()'.magenta);
-        console.log('      \'----------------------\''.magenta);
-        console.log('                                '.magenta);
+    console.log('      _______________________'.magenta);
+    console.log('()===(                      (@===()'.magenta);
+    console.log('     \'______________________\'|'.magenta);
+    console.log('       |                     |'.magenta);
+    console.log('       |  Timo Plato         |'.magenta);
+    console.log('       |  is                 |'.magenta);
+    console.log('       |  now                |'.magenta);
+    console.log('       |  running!           |'.magenta);
+    console.log('       |                     |'.magenta);
+    console.log('       |                     |'.magenta);
+    console.log('       |             love,   |'.magenta);
+    console.log('       |           Joanne    |'.magenta);
+    console.log('      _)_____________________|'.magenta);
+    console.log('()===(                      (@===()'.magenta);
+    console.log('      \'----------------------\''.magenta);
+    console.log('                                '.magenta);
 
 
-        console.log('Check out the app at http://localhost:' + server.address().port);
-        console.log('Press Ctrl+C to stop.');
-    }
+    console.log('Check out the app at http://localhost:' + server.address().port);
+    console.log('Press Ctrl+C to stop.');
 });
